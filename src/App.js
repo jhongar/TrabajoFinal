6ed9a0da8cd028b508db77d9c2eaa2af60a161c7
jhongar <jotica_94@hotmail.com>
2023-05-13 13:49:27 -0500

    se agraga carrusel al footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Jheader from './components/header';
 import Jfooter from './components/footer';
